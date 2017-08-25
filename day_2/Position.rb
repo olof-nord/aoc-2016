cb41eaeb40ad67359163a0749a00ba95324dbafa
Olof Nord <olof_nord@zoho.com>
2017-08-25 18:04:45 +0200

    Adding file input functionality

--- day_2/Position.rb
+++ day_2/Position.rb
@@ -21,6 +21,15 @@ class Position
     move(0, 1)
   end
 
+  def to_s
+    translations = {
+      [0,0] => "1", [0,1] => "2", [0,2] => "3",
+      [1,0] => "4", [1,1] => "5", [1,2] => "6",
+      [2,0] => "7", [2,1] => "8", [2,2] => "9"
+    }
+    translations[self.coordinates]
+  end
+
   def move(x, y)
     if inrange(self.coordinates[0] + x, self.coordinates[1] + y)
       self.coordinates[0] += x
@@ -30,13 +39,11 @@ class Position
 
   def inrange(x, y)
     if ((0..2) === x && (0..2) === y)
-      #print "DEBUG: inrange: true \n"
       return true
     else
-      #print "DEBUG: inrange: false \n"
       return false
     end
   end
 
-  private :inrange
+  private :inrange, :move
 end
--- day_2/program.rb
+++ day_2/program.rb
@@ -4,38 +4,25 @@
 # https://adventofcode.com/2016/day/2
 
 require "./Position.rb"
+$results = ""
+$pos = Position.new
 
-pos = Position.new()
+File.foreach('./inputs.txt') do |line|
+  line.each_char do |char|
+    case char
+    when "U"
+      $pos.up
+    when "D"
+      $pos.down
+    when "R"
+      $pos.right
+    when "L"
+      $pos.left
+    else
+      $results.concat($pos.to_s)
+    end
+  end
+end
 
 puts "AoC Day 2: Bathroom Security"
-print "Starting position: ", pos.coordinates,"\n"
-
-# ULL
-pos.up
-pos.left
-pos.left
-print "First position: ", pos.coordinates,"\n"
-
-# RRDDD
-pos.right
-pos.right
-pos.down
-pos.down
-pos.down
-print "Second position: ", pos.coordinates,"\n"
-
-# LURDL
-pos.left
-pos.up
-pos.right
-pos.down
-pos.left
-print "Third position: ", pos.coordinates,"\n"
-
-# UUUUD
-pos.up
-pos.up
-pos.up
-pos.up
-pos.down
-print "Fourth position: ", pos.coordinates,"\n"
+print "Code is: ", $results, "\n"
